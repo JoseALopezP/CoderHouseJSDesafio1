41c880a715554de7fc190ffb956bf190dc093ec9
JoseALopezP <jalp42@live.com>
2022-07-04 16:23:20 -0300

    Added option for developer UX

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//funcion constructora
 function Producto(codigo, marca, tipo, peso, precio){
     this.codigo = codigo;
     this.marca = marca;
@@ -8,19 +9,8 @@ function Producto(codigo, marca, tipo, peso, precio){
     this.cambiarPrecio = (nuevoPrecio) => (this.precio = nuevoPrecio);
 }
 
-const productos=[
-    // new Producto(1, "La Paulina", "Queso Cremoso tradicional", 100, 86.3),
-    // new Producto(2, "La Paulina", "Queso Port Salud tradicional", 100, 98.2),
-    // new Producto(3, "La Paulina", "Queso Reggianito", 100, 212.7),
-    // new Producto(4, "La Paulina", "Queso Crema", 100, 78.1),
-    // new Producto(5, "Paladini", "Mortadela", 100, 82.8),
-    // new Producto(6, "Paladini", "Jamon Cocido", 100, 126.1),
-    // new Producto(7, "Paladini", "Bondiola", 100, 213.2),
-    // new Producto(8, "Paladini", "Salame Milan", 100, 156.7),
-    // new Producto(9, "Paladini", "Queso Cremoso", 100, 97.5),
-    // new Producto(10, "Paladini", "Queso Reggianito", 100, 191.9),
-    // new Producto(11, "Paladini", "Queso Danbo", 100, 108.7)
-];
+//declaracion del array de productos
+const productos=[];
 
 //Agregando productos basicos
 let newProducto = new Producto(1, "La Paulina", "Queso Cremoso tradicional", 100, 86.3)
@@ -44,6 +34,7 @@ productos.push(newProducto);
 newProducto = new Producto(10, "Paladini", "Queso Danbo", 100, 108.7);
 productos.push(newProducto);
 
+//simulador interactivo
 function carrito(){
     let carrito = 0;
     let x;
@@ -75,6 +66,12 @@ function option(){
     return x;
 }
 
+function option(){
+    let x;
+    x = prompt('Es usted un cliente? (C = cliente, D = Desarrollador)');
+    return x;
+}
+
 function checkOption(x){
     while(x != "S" && (parseInt(x) > produtos.length || parseInt(x) < 1)){
         alert('Valor incorrecto');
@@ -92,4 +89,6 @@ function listprodcutos(){
     );
 }
 
+
+let x = optionI();
 carrito();
