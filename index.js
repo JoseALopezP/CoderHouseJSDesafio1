8b1dfd7e57b6bb03cfdaa2ecab6030a17d269aa8
JoseALopezP <jalp42@live.com>
2022-07-04 16:09:30 -0300

    Using push for every object in array. Changed for iterations for forEach when possible

--- index.js
+++ index.js
@@ -8,19 +8,41 @@ function Producto(codigo, marca, tipo, peso, precio){
     this.cambiarPrecio = (nuevoPrecio) => (this.precio = nuevoPrecio);
 }
 
-let productos=[
-    new Producto(1, "La Paulina", "Queso Cremoso tradicional", 100, 86.3),
-    new Producto(2, "La Paulina", "Queso Port Salud tradicional", 100, 98.2),
-    new Producto(3, "La Paulina", "Queso Reggianito", 100, 212.7),
-    new Producto(4, "La Paulina", "Queso Crema", 100, 78.1),
-    new Producto(5, "Paladini", "Mortadela", 100, 82.8),
-    new Producto(6, "Paladini", "Jamon Cocido", 100, 126.1),
-    new Producto(7, "Paladini", "Bondiola", 100, 213.2),
-    new Producto(8, "Paladini", "Salame Milan", 100, 156.7),
-    new Producto(9, "Paladini", "Queso Cremoso", 100, 97.5),
-    new Producto(10, "Paladini", "Queso Reggianito", 100, 191.9),
-    new Producto(11, "Paladini", "Queso Danbo", 100, 108.7)
+const productos=[
+    // new Producto(1, "La Paulina", "Queso Cremoso tradicional", 100, 86.3),
+    // new Producto(2, "La Paulina", "Queso Port Salud tradicional", 100, 98.2),
+    // new Producto(3, "La Paulina", "Queso Reggianito", 100, 212.7),
+    // new Producto(4, "La Paulina", "Queso Crema", 100, 78.1),
+    // new Producto(5, "Paladini", "Mortadela", 100, 82.8),
+    // new Producto(6, "Paladini", "Jamon Cocido", 100, 126.1),
+    // new Producto(7, "Paladini", "Bondiola", 100, 213.2),
+    // new Producto(8, "Paladini", "Salame Milan", 100, 156.7),
+    // new Producto(9, "Paladini", "Queso Cremoso", 100, 97.5),
+    // new Producto(10, "Paladini", "Queso Reggianito", 100, 191.9),
+    // new Producto(11, "Paladini", "Queso Danbo", 100, 108.7)
 ];
+
+//Agregando productos basicos
+let newProducto = new Producto(1, "La Paulina", "Queso Cremoso tradicional", 100, 86.3)
+productos.push(newProducto);
+newProducto = new Producto(2, "La Paulina", "Queso Reggianito", 100, 212.7);
+productos.push(newProducto);
+newProducto = new Producto(3, "La Paulina", "Queso Crema", 100, 78.1);
+productos.push(newProducto);
+newProducto = new Producto(4, "Paladini", "Mortadela", 100, 82.8);
+productos.push(newProducto);
+newProducto = new Producto(5, "Paladini", "Jamon Cocido", 100, 126.1);
+productos.push(newProducto);
+newProducto = new Producto(6, "Paladini", "Bondiola", 100, 213.2);
+productos.push(newProducto);
+newProducto = new Producto(7, "Paladini", "Salame Milan", 100, 156.7);
+productos.push(newProducto);
+newProducto = new Producto(8, "Paladini", "Queso Cremoso", 100, 97.5);
+productos.push(newProducto);
+newProducto = new Producto(9, "Paladini", "Queso Reggianito", 100, 191.9);
+productos.push(newProducto);
+newProducto = new Producto(10, "Paladini", "Queso Danbo", 100, 108.7);
+productos.push(newProducto);
 
 function carrito(){
     let carrito = 0;
@@ -49,22 +71,25 @@ function cantidad(){
 
 function option(){
     let x;
-    x = prompt('¿Qué desea agregar al carrito? (1 al 11, S para salir)');
+    x = prompt('¿Qué desea agregar al carrito? (1 al ' + productos.length + ', S para salir)');
     return x;
 }
 
 function checkOption(x){
-    while(x != "S" && (parseInt(x) > 11 || parseInt(x) < 1)){
+    while(x != "S" && (parseInt(x) > produtos.length || parseInt(x) < 1)){
         alert('Valor incorrecto');
         x = option();
     }
     return x;
 }
 
+//Funcion para recorrer la funcion descirbir en cada producto
 function listprodcutos(){
-    for (let i=0; i<11; i++){
-        productos[i].describir()
-    }
+    productos.forEach(
+        function callback(v , i){ //usado para atrapar el 2do valor que envia el forEach (index)
+            productos[i].describir();
+        }
+    );
 }
 
 carrito();
